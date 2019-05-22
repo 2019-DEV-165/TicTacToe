61b79442ae7dd031f2007dc31aed6b0039f88196
2019dev165 <2019dev165@gmail.com>
2019-05-22 11:59:26 +0530

    Added testcase to swap from player X to Player O after move successfully stored, refactor the view model correspondingly

--- app/src/main/java/com/katatictactoe/TicTacToeViewModel.kt
+++ app/src/main/java/com/katatictactoe/TicTacToeViewModel.kt
@@ -7,18 +7,40 @@ class TicTacToeViewModel : ViewModel() {
     private var mCurrentPlayer = Player.PLAYER_X_ID
     private var mGameBoard = Array(3) { IntArray(3) }
 
+    private fun getGameBoard(): Array<IntArray> {
+        return mGameBoard
+    }
+
+    private fun isIndexAvailableForPlayerMove(index: Int) = getGameBoardByIndex(index) == 0
+
+    private fun updateGameBoardIndex(position: Int, playerTag: Int) {
+        getGameBoard()[position / 3][position % 3] = playerTag
+    }
+
+    private fun updateCurrentPlayer(playerTag: Int) {
+        mCurrentPlayer = when (playerTag) {
+            Player.PLAYER_X_ID -> Player.PLAYER_O_ID
+            else -> Player.PLAYER_X_ID
+        }
+    }
+
+
     fun getCurrentPlayer(): Int {
         return mCurrentPlayer
     }
 
     fun storePlayerMoves(@GameBoardPosition.Position index: Int) {
-        getGameBoard()[index / 3][index % 3] = getCurrentPlayer()
+        if (isIndexAvailableForPlayerMove(index)) {
+            updateGameBoardIndex(index, getCurrentPlayer())
+            updateCurrentPlayer(getCurrentPlayer())
+        }
     }
 
     fun getGameBoardByIndex(position: Int) = getGameBoard()[position / 3][position % 3]
 
-    private fun getGameBoard(): Array<IntArray> {
-        return mGameBoard
+    fun resetGameBoard() {
+        mCurrentPlayer = Player.PLAYER_X_ID
+        mGameBoard = Array(3) { IntArray(3) }
     }
 
 }
--- app/src/test/java/com/katatictactoe/TicTacToeViewModelUnitTest.kt
+++ app/src/test/java/com/katatictactoe/TicTacToeViewModelUnitTest.kt
@@ -1,12 +1,19 @@
 package com.katatictactoe
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 
 class TicTacToeViewModelUnitTest {
 
     private var ticTacToeViewModel = TicTacToeViewModel()
+
+    @Before
+    fun resetGameBoard() {
+        ticTacToeViewModel.resetGameBoard()
+    }
+
 
     @Test
     fun testShouldCheckWhetherTheFirstMoveAvailableForPlayerX() {
@@ -18,5 +25,15 @@ class TicTacToeViewModelUnitTest {
         assertNotNull(ticTacToeViewModel.storePlayerMoves(GameBoardPosition.INDEX_TOP_MIDDLE))
         assertSame(ticTacToeViewModel.getGameBoardByIndex(GameBoardPosition.INDEX_TOP_MIDDLE), Player.PLAYER_X_ID)
     }
+
+    @Test
+    fun testShouldCheckSwappingOfPlayerXtoPlayerOAfterMoveSuccessfullyStored() {
+        assertEquals(ticTacToeViewModel.getCurrentPlayer(), Player.PLAYER_X_ID)
+        ticTacToeViewModel.storePlayerMoves(GameBoardPosition.INDEX_TOP_MIDDLE)
+        assertEquals(ticTacToeViewModel.getCurrentPlayer(), Player.PLAYER_O_ID)
+        assertNotEquals(ticTacToeViewModel.getCurrentPlayer(), Player.PLAYER_X_ID)
+
+    }
+
 
 }
